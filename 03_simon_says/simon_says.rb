f5ac98a7667fd40aac5c9c4042714c2f14c0ab79
Phuc Le Dien <phucledien@gmail.com>
2017-07-19 12:04:18 +0700

    fix bug simon_says.rb

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
-#write your code here
+def echo(say)
+  say
+end
+
+def shout(say)
+  say.upcase
+end
+
+def repeat(say, number=2)
+  ([say] * number).join(" ")
+end
+
+def start_of_word(say, number)
+  say[0...number]
+end
+
+def first_word(say)
+  say.split(' ')[0]
+end
+
+def titleize(say)
+  little_words = ['and', 'the', 'over']
+  say_array = say.split.map do |word|
+    if little_words.include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  say_array[0].capitalize!
+  say_array.join(' ')
+end
