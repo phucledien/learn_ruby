40eaa33d980061dae5223eec1559d432146dd945
Phuc Le Dien <phucledien@gmail.com>
2017-07-19 16:44:20 +0700

    fix bug pig_latin.rb

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+def translate (string)
+  string_array = string.split
+  string_array.map! do |s|
+    if s.index(/\w*qu/) == 0
+      length = s[/\w*qu/].length
+      s = s[length..-1] + s[0, length] + 'ay'
+    elsif %w(a e i o u).include?(s[0])
+      s = s + 'ay'
+    else
+      length = s[/[^aeiou]*/].length
+      s = s[length..-1] + s[0, length] + 'ay'
+    end
+  end
+  string_array.join(' ')
+end
