86380c71c5097a0cfcb133e93fac928baa817d6e
Phuc Le Dien <phucledien@gmail.com>
2017-07-19 09:23:22 +0700

    fix bug calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,29 @@
-#write your code here
+def add (a, b)
+  a+b
+end
+
+def subtract (a, b)
+  a-b
+end
+
+def sum (array)
+  sum = 0
+  array.each { |num| sum = sum + num  }
+  sum
+end
+
+def multiply (a, b)
+  a*b
+end
+
+def power (a, b)
+  a**b
+end
+
+def factorial (a)
+  fact = 1
+  1.upto(a) do |i|
+    fact = fact * i
+  end
+  fact
+end
